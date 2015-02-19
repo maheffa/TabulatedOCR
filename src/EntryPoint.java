173fb85fce62ab40d0f1293e6db9046990447c73
xseni <maheffa32@gmail.com>
2015-02-19 13:13:07 +0300

    New project

---
+++ src/EntryPoint.java
@@ -0,0 +1,21 @@
+// File:    EntryPoint.java
+// Created: 19/02/2015
+
+import org.opencv.core.Core;
+
+/**
+ * @author mahefa
+ */
+public class EntryPoint {
+
+    public EntryPoint() {
+    }
+
+    static {
+        System.load(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
