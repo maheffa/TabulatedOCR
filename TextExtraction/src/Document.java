c0e5ed76ff0b0379af7db8427c912137d6e4e810
xseni <maheffa32@gmail.com>
2015-04-15 18:31:21 +0300

    Created Document class, before reverting Binarization algorithm to kmeans algorithm using encog library

--- TextExtraction/src/Document.java
+++ TextExtraction/src/Document.java
@@ -1,6 +1,8 @@
 // File:    Document.java
 // Created: 14/04/2015
 
+import java.util.ArrayList;
+
 /**
  * @author mahefa
  */
@@ -8,13 +10,30 @@ public class Document {
     private String path;
     private BinaryImage binaryImage;
     private ConnectedPixel connectedPixel;
+    private ArrayList<ConnectedPixel> connectedPixels;
+    private ArrayList<CharacterPixel> characterPixels;
 
     public Document(String path) {
         this.path = path;
         this.binaryImage = new BinaryImage(path);
     }
 
+    public void detectCharacters(int radius, int margin, int minCharSize, int maxCharSize, int charSize) {
+        connectedPixels = new ArrayList<ConnectedPixel>();
+        characterPixels = new ArrayList<CharacterPixel>();
+        for (ConnectedPixel cp : ConnectedPixel.getConnectedPixels(radius, margin, binaryImage)) {
+            if (cp.getHeight() >= minCharSize
+                    && cp.getHeight() <= maxCharSize
+                    && cp.getWidth() >= minCharSize
+                    && cp.getWidth() <= maxCharSize) {
+                connectedPixels.add(cp);
+                characterPixels.add(cp.createCharaterPixel());
+            }
+        }
+    }
 
-
+    public void detectStructure() {
+        // TODO
+    }
 
 }
